daa00afaa975fcfecbe01b0ecb6a3c5a12df4842
GPointExtreme <dominik.geiger@hotmail.com>
2018-01-06 13:50:42 +0100

    Client update für Eventausgabe und Fehlerfall

--- client/listEvents.js
+++ client/listEvents.js
@@ -11,9 +11,32 @@ if (process.argv.length < 3) {
     user = process.argv[2];
 }
 
+//G: ab hier kopiert...
+Request.get(hostUrl + '/users/?name=' + user, userResponse);
+
+function userResponse(error, response, body) {
+	let users = JSON.parse(body);
+	if (users.length === 0) {
+		console.log('Keine Person mit diesem Namen gefunden.');
+		process.exit(0);
+	}
+	let user = users[0]; // wir nehmen nur erstes Element aus Array
+	Request.get(hostUrl + '/events/?participant=' + user.$loki, eventResponse);
+}
+
+function eventResponse(error, response, body) {
+	let events = JSON.parse(body);
+	events.sort(compareEventTime).map(printEvent);
+}
+
 // Hilfsfunktion zum Sortieren nach Uhrzeit
 function compareEventTime(ev1, ev2) {
     let time1 = Date.parse(ev1.startTime);
     let time2 = Date.parse(ev2.startTime);
     return (time1 - time2);
+}
+
+// Hilfsfunktion zur Ausgabe eines Events
+function printEvent(event) {
+	console.log(event.startTime + " - " + event.name + " @ " + event.place);
 }
